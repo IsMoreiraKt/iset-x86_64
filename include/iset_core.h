ed96c8ee947284084900e94f97e1fef7ea908154
IsMoreiraKt <ismaelmoreiraubuntu@gmail.com>
2024-12-07 03:19:55 +0000

    refactor: Modification of base functions.

--- include/iset_core.h
+++ include/iset_core.h
@@ -83,70 +83,10 @@ unsigned char OR(
  * Return:
  * - The result of the NOT operation (negation of the operand).
  */
-unsigned char NOT(
-    unsigned char first_argument
+char NOT(
+    char first_argument
 ) {
-    return !first_argument;
-}
-
-
-/*
- * NAND Function:
- * Performs the "NAND" logical operation between two operands.
- * Returns 1 if at least one of the operands is 0.
- * 
- * Parameters:
- * - first_argument: The first operand (must be 0 or 1).
- * - second_argument: The second operand (must be 0 or 1).
- * 
- * Return:
- * - The result of the NAND operation between the two operands.
- */
-unsigned char NAND(
-    unsigned char first_argument, 
-    unsigned char second_argument
-) {
-    return !(first_argument & second_argument);
-}
-
-
-/*
- * NOR Function:
- * Performs the "NOR" logical operation between two operands.
- * Returns 1 only if both operands are 0.
- * 
- * Parameters:
- * - first_argument: The first operand (must be 0 or 1).
- * - second_argument: The second operand (must be 0 or 1).
- * 
- * Return:
- * - The result of the NOR operation between the two operands.
- */
-unsigned char NOR(
-    unsigned char first_argument, 
-    unsigned char second_argument
-) {
-    return !(first_argument | second_argument);
-}
-
-
-/*
- * XNOR Function:
- * Performs the "XNOR" logical operation between two operands.
- * Returns 1 if the operands are the same, and 0 if they are different.
- * 
- * Parameters:
- * - first_argument: The first operand (must be 0 or 1).
- * - second_argument: The second operand (must be 0 or 1).
- * 
- * Return:
- * - The result of the XNOR operation between the two operands.
- */
-unsigned char XNOR(
-    unsigned char first_argument, 
-    unsigned char second_argument
-) {
-    return !(first_argument ^ second_argument);
+    return ~first_argument;
 }
 
 
