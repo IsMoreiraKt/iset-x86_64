75f67139fa011fde478eb42aa3a3885bfbed2a4e
IsMoreiraKt <ismaelmoreiraubuntu@gmail.com>
2024-12-07 02:06:12 +0000

    refactor: Restructuring of addition functions to use carry flags.

--- src/int/8086_8088/add.c
+++ src/int/8086_8088/add.c
@@ -21,16 +21,15 @@ void ADD(
     unsigned short second_argument
 ) {
     unsigned short sum = 0;
-    unsigned short carry = 0;
 
     // Loop through each bit (0 to 15) of the operands
     for (unsigned short i = 0; i <= 16; i++) {
         unsigned short x_bit = AND((*first_argument >> i), 1);  // Get the i-th bit of the first operand
         unsigned short y_bit = AND((second_argument >> i), 1); // Get the i-th bit of the second operand
-        unsigned short sum_bit = XOR(XOR(x_bit, y_bit), carry); // Calculate the sum bit
+        unsigned short sum_bit = XOR(XOR(x_bit, y_bit), CARRY_FLAG); // Calculate the sum bit
 
         // Calculate the carry for the next iteration (if any)
-        carry = OR(AND(x_bit, y_bit), AND(carry, XOR(x_bit, y_bit)));
+        CARRY_FLAG = OR(AND(x_bit, y_bit), AND(CARRY_FLAG, XOR(x_bit, y_bit)));
 
         // Combine the sum bit into the final sum
         sum |= (sum_bit << i);
@@ -59,16 +58,15 @@ void ADDL(
     unsigned int second_argument
 ) {
     unsigned int sum = 0;
-    unsigned int carry = 0;
 
     // Loop through each bit (0 to 31) of the operands
     for (unsigned int i = 0; i <= 32; i++) {
         unsigned int x_bit = AND((*first_argument >> i), 1);  // Get the i-th bit of the first operand
         unsigned int y_bit = AND((second_argument >> i), 1); // Get the i-th bit of the second operand
-        unsigned int sum_bit = XOR(XOR(x_bit, y_bit), carry); // Calculate the sum bit
+        unsigned int sum_bit = XOR(XOR(x_bit, y_bit), CARRY_FLAG); // Calculate the sum bit
 
         // Calculate the carry for the next iteration (if any)
-        carry = OR(AND(x_bit, y_bit), AND(carry, XOR(x_bit, y_bit)));
+        CARRY_FLAG = OR(AND(x_bit, y_bit), AND(CARRY_FLAG, XOR(x_bit, y_bit)));
 
         // Combine the sum bit into the final sum
         sum |= (sum_bit << i);
