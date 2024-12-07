1c8e26d0835c6c0969a953620d2e12d05f3661a6
IsMoreiraKt <ismaelmoreiraubuntu@gmail.com>
2024-12-07 02:05:27 +0000

    feat: Creation of the file that manages global parameters.

---
+++ src/global.c
@@ -0,0 +1,10 @@
+#include "iset_core.h"
+
+
+
+/*
+ * CARRY_FLAG:
+ * Global variable indicating if a carry occurred during an arithmetic operation.
+ * Initially set to 0 (no carry).
+ */
+unsigned char CARRY_FLAG = 0;
